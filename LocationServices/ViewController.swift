f88a58d7cdc0658d299a3a6065063cbcd4fbfc74
Itreau Bigsby <ibigsby@asu.edu>
2020-10-17 17:48:27 -0700

    Initial Commit

---
+++ LocationServices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationServices
+//
+//  Created by  on 10/17/20.
+//  Copyright © 2020 asu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
